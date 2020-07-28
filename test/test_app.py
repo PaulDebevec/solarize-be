a5c3fb0679967aee021a41230eb3407e8839037d
Jordan Sewell <jrsewell400@gmail.com>
2020-07-28 15:54:02 -0600

    Add tests for NREL object and associated functions

--- test/test_app.py
+++ test/test_app.py
@@ -1,30 +1,9 @@
-import requests
-import json
 from flask import Flask, request, jsonify
-import os
+from app import NREL
 from dotenv import load_dotenv
+import requests
+import os
 load_dotenv()
-
-app = Flask(__name__)
-app.config['TESTING'] = True
-
-
-def pretty_print_request(request):
-    print('\n{}\n{}\n\n{}\n\n{}\n'.format(
-        '-----------Request----------->',
-        request.method + ' ' + request.url,
-        '\n'.join('{}: {}'.format(k, v) for k, v in request.headers.items()),
-        request.body)
-    )
-
-
-def pretty_print_response(response):
-    print('\n{}\n{}\n\n{}\n\n{}\n'.format(
-        '<-----------Response-----------',
-        'Status code:' + str(response.status_code),
-        '\n'.join('{}: {}'.format(k, v) for k, v in response.headers.items()),
-        response.text)
-    )
 
     
 def test_one_equals_one():
@@ -33,7 +12,7 @@ def test_one_equals_one():
     assert a == b
     
     
-def test_get_nrel_request_and_response():
+def test_get_system_info():
     url = f"https://developer.nrel.gov/api/pvwatts/v6.json?api_key={os.getenv('NREL_API_KEY')}&address=155+beryl+way+broomfield&system_capacity=4&azimuth=180&tilt=40&array_type=1&module_type=1&losses=10"
 
     response = requests.get(url)
@@ -60,29 +39,87 @@ def test_get_nrel_request_and_response():
     assert response_body["outputs"]["ac_monthly"][1] == 493.1632385253906
     assert response_body["outputs"]["ac_monthly"][2] == 599.2940673828125
 
-    # pretty_print_request(response.request)
-    # pretty_print_response(response)
+
+def test_all_functions():
+    fe_json = {
+          "solarizer_parameters": {
+            "name": "example request",
+            "address": "1490+Delgany+St+Denver+CO+80202",
+            "system_capacity": 4,
+            "module_type": 0,
+            "array_type": 0,
+            "losses": 14,
+            "tilt": 20,
+            "azimuth": 180
+          },
+          "historical_kWh": {
+            "january": 100,
+            "february": 100,
+            "march": 100,
+            "april": 100,
+            "may": 100,
+            "june": 100,
+            "july": 100,
+            "august": 100,
+            "september": 100,
+            "october": 100,
+            "november": 100,
+            "december": 100
+          }
+        }
+    results = NREL(fe_json)
+    results.get_system_info()
+    kwh_rate_info = results.get_kwh_rate_info()
+    results.calculate_value(kwh_rate_info)
+    assert results.monthly_values["value_monthly"] == [
+        43.872415115356446,
+        47.50345698547363,
+        63.055829528808594,
+        66.16632629394532,
+        67.38588641357421,
+        67.31052465820312,
+        66.28732055664062,
+        64.52443954467773,
+        60.22124554443359,
+        53.6966434173584,
+        45.53072444152832,
+        40.525810928344725
+    ]
+    assert len(results.monthly_values["value_monthly"]) == 12
+    assert type(results.monthly_values["value_monthly"][0]) == float
+
+    results.calculate_offset()
+    assert results.percent_offset["percent_offset"] == 517.4062016805012
 
 
-# def test_get_solar_system():
-#
-#
-#     # def echo():
-#     #     return jsonify(flask.request.get_json())
-#
-#     with app.test_client() as c:
-#         req = c.post('/results', json={"address": "155 beryl way broomfield",
-#                                        "system_capacity": "4",
-#                                        "azimuth": "180",
-#                                        "tilt": "40",
-#                                        "array_type": "1",
-#                                        "module_type": "1",
-#                                        "losses": "10"})
-#         assert isinstance(req, str)
-#         a = json.loads(req)
-#         json_data = a.get_data()
-#
-#
-#         assert json_data["outputs"]["ac_monthly"][0] == 479.64813232421875
-#         assert json_data["outputs"]["ac_monthly"][1] == 493.1632385253906
-#         assert json_data["outputs"]["ac_monthly"][2] == 599.2940673828125
+def test_calculate_offset_returns_empty_string():
+    fe_json = {
+        "solarizer_parameters": {
+            "name": "example request",
+            "address": "1490+Delgany+St+Denver+CO+80202",
+            "system_capacity": 4,
+            "module_type": 0,
+            "array_type": 0,
+            "losses": 14,
+            "tilt": 20,
+            "azimuth": 180
+        },
+        "historical_kWh": {
+            "january": "undefined",
+            "february": 100,
+            "march": 100,
+            "april": 100,
+            "may": 100,
+            "june": 100,
+            "july": 100,
+            "august": 100,
+            "september": 100,
+            "october": 100,
+            "november": 100,
+            "december": 100
+        }
+    }
+    results = NREL(fe_json)
+    results.get_system_info()
+    results.calculate_offset()
+    assert results.percent_offset["percent_offset"] == ""
